46f6c339f6c6caf0e18fe83335a35fbcc9608b7f
JorgeHerMiranda2 <jorgeduardo.hernandez@gmail.com>
2024-10-16 00:04:08 -0600

    tarea 12

---
+++ BusquedaBinaria.py
@@ -0,0 +1,16 @@
+def BusquedaBinaria(A,elemento):
+#input - arreglo A de elementos ordenados 
+#output - indice si se encuentra el elemento, -1 si no se encuentra 
+
+A[0]:
+elemento = 0 
+        
+    
+for in range[i(elemento)]:
+ return elemento
+if A[m]= elemento 
+return elemento
+if A[m]<elemento
+BusquedaBinaria(A (i),elemento)
+else 
+return -1
