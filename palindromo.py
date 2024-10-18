ba9a713be602bd3ec68833c1e2e1b2c752647c50
JorgeHerMiranda2 <jorgeduardo.hernandez@gmail.com>
2024-10-18 00:51:57 -0600

    tarea 13

---
+++ palindromo.py
@@ -0,0 +1,14 @@
+def palindromo(numero):
+    if numero < 0:
+        return False
+        
+        
+    numero_prin = numero 
+    numero_reversa = 0
+    
+    digitos = 0
+    for num in 0:
+        digitos +=1
+        numero /=10
+        
+      
